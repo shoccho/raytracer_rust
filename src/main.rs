00e1c7e430fcb8501221ba7db33746af1ddfe6da
shoccho <sfb.shoccho@gmail.com>
2024-09-22 00:45:58 +0600

    Implemented circle hit, refactored code

--- src/main.rs
+++ src/main.rs
@@ -13,8 +13,9 @@ use vec3::Vec3;
 
 use std::time::Duration;
 
-const IMAGE_HEIGHT: usize = 256;
-const IMAGE_WIDTH: usize = 256;
+const ASPECT_RATIO: f64 = 16.0 / 9.0;
+const IMAGE_WIDTH: usize = 400;
+const IMAGE_HEIGHT: usize = (IMAGE_WIDTH as f64 / ASPECT_RATIO) as usize;
 
 fn print_head() {
     println!("P3");
@@ -25,7 +26,7 @@ fn print_head() {
 fn print_image(buff_data: &mut [Vec<Vec3>]) {
     for (j, row) in buff_data.iter().enumerate() {
         eprintln!("Lines remaining {}", IMAGE_HEIGHT - j);
-        for data in row.iter(){
+        for data in row.iter() {
             let ir = (255.999 * data.x) as isize;
             let ig = (255.999 * data.y) as isize;
             let ib = (255.999 * data.z) as isize;
@@ -34,43 +35,40 @@ fn print_image(buff_data: &mut [Vec<Vec3>]) {
     }
 }
 
+fn hit_sphere(center: &Vec3, radius: f64, ray: &Ray) -> bool {
+    let oc = Vec3::sub(center, &ray.origin);
+    let a = Vec3::dot(&ray.direction, &ray.direction);
+    let b = -2.0 * Vec3::dot(&ray.direction, &oc);
+    let c = Vec3::dot(&oc, &oc) - (radius * radius);
+    let d = (b * b) - (4.0 * a * c);
+    d >= 0.0
+}
+
 fn ray_color(ray: &Ray) -> Vec3 {
-    // Vec3::default()
-    // vec3 unit_direction = unit_vector(r.direction());
+    if hit_sphere(&Vec3::new(0.0, 0.0, -1.0), 0.5, ray) {
+        return Vec3::new(1.0, 0.0, 0.0);
+    }
+
     let unit_dir = Vec3::unit(&ray.direction);
 
     let a = 0.5 * (unit_dir.y + 1.0);
-    // return (1.0-a)*color(1.0, 1.0, 1.0) + a*color(0.5, 0.7, 1.0);
+
     Vec3::add(
-        &Vec3::mul(
-            &Vec3 {
-                x: 1.0,
-                y: 1.0,
-                z: 1.0,
-            },
-            1.0 - a,
-        ),
-        &Vec3::mul(
-            &Vec3 {
-                x: 0.5,
-                y: 0.7,
-                z: 1.0,
-            },
-            a,
-        ),
+        &Vec3::mul(&Vec3::new(1.0, 1.0, 1.0), 1.0 - a),
+        &Vec3::mul(&Vec3::new(0.5, 0.7, 1.0), a),
     )
 }
 
 fn main() {
     // print_head();
-    
+
     let mut buff_data = vec![vec![Vec3::default(); IMAGE_WIDTH]; IMAGE_HEIGHT];
 
     let sdl_context = sdl2::init().unwrap();
     let video_subsystem = sdl_context.video().unwrap();
 
     let window = video_subsystem
-        .window("rust-sdl2 demo", IMAGE_WIDTH as u32, IMAGE_WIDTH as u32)
+        .window("rust-sdl2 demo", IMAGE_WIDTH as u32, IMAGE_HEIGHT as u32)
         .position_centered()
         .build()
         .unwrap();
@@ -79,44 +77,26 @@ fn main() {
 
     let focal_length = 1.0;
     let viewport_height = 2.0;
-    let camera_center = Vec3 {
-        x: 0f64,
-        y: 0f64,
-        z: 0f64,
-    };
+    let camera_center = Vec3::new(0f64, 0f64, 0f64);
     let viewport_width = viewport_height * ((IMAGE_WIDTH) as f64 / IMAGE_HEIGHT as f64);
-    let viewport_u = Vec3 {
-        x: viewport_width,
-        y: 0f64,
-        z: 0f64,
-    };
-    let viewport_v = Vec3 {
-        x: 0f64,
-        y: -viewport_height,
-        z: 0f64,
-    };
+    let viewport_u = Vec3::new(viewport_width, 0f64, 0f64);
+    let viewport_v = Vec3::new(0f64, -viewport_height, 0f64);
 
     let pixel_delta_u = Vec3::div(&viewport_u, IMAGE_WIDTH as f64);
     let pixel_delta_v = Vec3::div(&viewport_v, IMAGE_HEIGHT as f64);
 
     let viewport_upper_left = Vec3::sub(
-        &camera_center,
-        &Vec3::sub(
-            &Vec3 {
-                x: 0.0,
-                y: 0.0,
-                z: focal_length,
-            },
-            &Vec3::sub(&Vec3::div(&viewport_u, 2.0), &Vec3::div(&viewport_v, 2.0)),
-        ),
+        &Vec3::sub(&camera_center, &Vec3::new(0.0, 0.0, focal_length)),
+        &Vec3::add(&Vec3::div(&viewport_u, 2.0), &Vec3::div(&viewport_v, 2.0)),
     );
+
     let pixel00_loc = Vec3::add(
         &viewport_upper_left,
         &Vec3::mul(&Vec3::add(&pixel_delta_u, &pixel_delta_v), 0.5),
     );
 
     for (j, row) in buff_data.iter_mut().enumerate() {
-        for (i, data) in row.iter_mut().enumerate(){
+        for (i, data) in row.iter_mut().enumerate() {
             let pixel_center = Vec3::add(
                 &pixel00_loc,
                 &Vec3::add(
@@ -129,11 +109,10 @@ fn main() {
                 origin: camera_center.clone(),
                 direction: ray_direction.clone(),
             };
-            let ray_color = ray_color(&ray);
-            data.x = ray_color.x;
-            data.y = ray_color.y;
-            data.z = ray_color.z;
-            
+            let color = ray_color(&ray);
+            data.x = color.x;
+            data.y = color.y;
+            data.z = color.z;
         }
     }
 
@@ -157,7 +136,7 @@ fn main() {
             }
         }
         for (j, row) in buff_data.iter().enumerate() {
-            for (i, data) in row.iter().enumerate(){
+            for (i, data) in row.iter().enumerate() {
                 canvas.set_draw_color(Color::RGB(
                     (255.999 * data.x) as u8,
                     (255.999 * data.y) as u8,
